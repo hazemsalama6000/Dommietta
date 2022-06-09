b032b1809ac2e0d673c2bdf37db67a2301010c0f
hazem.salama6000 <hazem.salama6000@gmail.com>
2022-06-09 16:31:22 +0200

    company design display card

--- src/app/modules/hr/models/ICompanyDisplayData.ts
+++ src/app/modules/hr/models/ICompanyDisplayData.ts
@@ -1,21 +1,23 @@
 export interface ICompanyDisplayData {
-	
-    id: number,
-    code?: string,
-    companyName: string,
-    activity?: string,
-    address: string,
-    phoneNumber?: number,
-    logoWeb?: string,
-	logoPrint?:string,
-    hasDirectTransferForStocks?:boolean,
-    email?: string,
-    isActive?: boolean,
-    mobileUserNumber?: number,
-    state?: string,
-    region?: string,
-    managerName?: string,
-    managerPosition?: string,
-    roleName?: string
-
+	id: number,
+	code?: string,
+	companyName: string,
+	activity?: string,
+	address: string,
+	phoneNumber?: number,
+	logoWeb?: string,
+	logoPrint?: string,
+	hasDirectTransferForStocks?: boolean,
+	email?: string,
+	isActive?: boolean,
+	mobileUserNumber?: number,
+	state?: string,
+	region?: string,
+	managerName?: string,
+	managerPosition?: string,
+	roleName?: string,
+	commercialRecord: string,
+	taxCardNo: string,
+	wTax: string
+	vatTax: string
 }
