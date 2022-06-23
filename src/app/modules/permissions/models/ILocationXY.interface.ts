51a968869907795a81fe2f869508f88ef98b5cb6
hazem.salama6000 <hazem.salama6000@gmail.com>
2022-06-23 09:09:30 +0200

    user location history

--- src/app/modules/permissions/models/ILocationXY.interface.ts
+++ src/app/modules/permissions/models/ILocationXY.interface.ts
@@ -2,5 +2,6 @@ export interface ILocationXY {
 	x:number,
 	y:number,
 	empName:string,
-	date:string
+	date:string,
+	status?:boolean
 }
--- src/app/modules/permissions/permissions-routing.module.ts
+++ src/app/modules/permissions/permissions-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LookupIdNameComponent } from 'src/app/shared-module/Components/lookupId_name/lookupId_name.component';
 import { OnlineUsersComponent } from './components/onlineUsers/onlineUsers.component';
 import { UserConnectionLogsComponent } from './components/userConnectionLogs/user-connection-logs.component';
+import { UserLocationLogsComponent } from './components/userLocationLogs/user-Locations-logs.component';
 
 const routes: Routes = [
 	{path:'onlineUsers' , component:OnlineUsersComponent},
-	{path:'userconnectionlogs' , component:UserConnectionLogsComponent }
+	{path:'userconnectionlogs' , component:UserConnectionLogsComponent },
+	{path:'userLocationLogs' , component:UserLocationLogsComponent}
 ];
 
 @NgModule({
